ec8559b0adf316662def8449b3afe18c50e018aa
mo'tasem maher salem <33785213+motasemmaher@users.noreply.github.com>
2025-04-25 01:34:56 +0300

    linting

--- blog-platform-fe/src/app/posts/page.tsx
+++ blog-platform-fe/src/app/posts/page.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { postsApi } from '@/lib/api/posts';
 import PostsIndex from '@/components/posts';
 
-
 export default async function PostsPage({searchParams}: {searchParams: Promise<{search: string, page: string} >}) {
   const {search, page} = await searchParams;
 
